bc784c2614c213fe0b0f6dd61b33ac7aaa4fec63
b-husein <bajrektarevic.husein@gmail.com>
2021-01-16 08:50:26 +0100

    updated

---
+++ 11_[sets]/add_pop.py
@@ -0,0 +1,12 @@
+
+# Consider the following code
+
+a = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+b = set(a)
+b.add(5)
+b.pop()
+
+# After executing this code, will the number 5 be a part of the set b?
+
+# Maybe it will, because the pop() method will 'pop out' random number and it can be 5.
+
---
+++ 11_[sets]/output.py
@@ -0,0 +1,12 @@
+
+# Write output of the following code
+
+a = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+b = set(a)
+print(len(a) - len(b))
+
+# len(a) we have 10 
+# b set(a) we'll get 4
+# print(len(a) - len(b)) it would be like 10 - 4
+# output is 6
+
