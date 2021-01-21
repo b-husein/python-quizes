49cd0715e45186117f78d72117732fdbb2558964
b-husein <bajrektarevic.husein@gmail.com>
2021-01-21 09:03:29 +0100

    updated

---
+++ 14_conditional_statements/phone_balance.py
@@ -0,0 +1,11 @@
+
+#First Example - try changing the value of phone_balance
+phone_balance = 5
+bank_balance = 50
+
+if phone_balance < 10:
+    phone_balance += 10
+    bank_balance -= 10
+
+print(phone_balance)
+print(bank_balance)
