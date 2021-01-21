d4c7e4f3698963679b76ac8ec5e0116234934a51
b-husein <bajrektarevic.husein@gmail.com>
2021-01-21 09:04:36 +0100

    updated

---
+++ 14_conditional_statements/number.py
@@ -0,0 +1,7 @@
+#Second Example - try changing the value of number
+
+number = 160
+if number % 2 == 0:
+    print("Number " + str(number) + " is even.")
+else:
+    print("Number " + str(number) + " is odd.")
